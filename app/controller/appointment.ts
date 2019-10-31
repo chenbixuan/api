d5a8a7601aaa906a6a47fa5fed570240fbca603d
colter <89688563@qq.com>
2019-10-31 23:34:58 +0800

    .

--- app/controller/appointment.ts
+++ app/controller/appointment.ts
@@ -1,9 +1,22 @@
 'use strict';
 
 import { Context } from 'egg';
+import { Op } from 'sequelize';
 
 export default class AppointmentController extends global.BaseController {
   constructor (ctx: Context) {
     super(ctx, ctx.service.appointment);
+  }
+
+  async index () {
+    const ctx = this.ctx;
+    const date = ctx.query.date;
+    if (date) {
+      ctx.query.status = {
+        [Op.in]: ['WAITING', 'FINISHED'],
+      };
+    }
+
+    await super.index();
   }
 }
